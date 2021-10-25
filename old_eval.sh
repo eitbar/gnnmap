61bb2d2c5a96667361489280a7e07926276200ca
tianzhoujin <1017901779@qq.com>
2021-10-25 17:36:26 +0800

    delete old py and sh file

--- old_eval.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=3 python3 old_eval.py "./data/en-hr/noc.wiki.30k.en-hr-aligned.EN.vec" "./data/en-hr/noc.wiki.30k.en-hr-aligned.HR.vec" -d ./data/en-hr/yacle.test.freq.2k.en-hr.tsv --retrieval csls --cuda --model ./data/en-hr/class.en-hr.ENHR-model.pickle
